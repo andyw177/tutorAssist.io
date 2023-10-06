48ba16e966b262a3535869b08111dc86da21ce90
andyw <andyw177@gmail.com>
2023-10-05 23:57:47 -0400

    add files

---
+++ TutorAssist/src/main/java/com/se785/TutorAssist/TutorAssistApplication.java
@@ -0,0 +1,13 @@
+package com.se785.TutorAssist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorAssistApplication.class, args);
+	}
+
+}
---
+++ TutorAssist/src/test/java/com/se785/TutorAssist/TutorAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se785.TutorAssist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
